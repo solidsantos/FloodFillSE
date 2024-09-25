4ef2cfa680cebba28e87bcff631678eb6d2e3531
Victor Santos <victor.eas19@gmail.com>
2024-09-25 17:58:13 -0300

    Vercel fixed

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
